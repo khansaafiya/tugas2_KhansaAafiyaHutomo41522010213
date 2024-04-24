98486cba43df5188eb05ecea77cadb0b9489d397
khansaafiya <41522010213@student.mercubuana.ac.id>
2024-04-24 17:09:22 +0700

    Update terbaru

--- app/src/main/java/com/example/tugas2login/tugas2register.java
+++ app/src/main/java/com/example/tugas2login/tugas2register.java
@@ -1,6 +1,10 @@
 package com.example.tugas2login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class tugas2register extends AppCompatActivity {
 
+    Button pindahlogin;
+    Button pindahregis;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tugas2register);
+        pindahlogin = findViewById(R.id.kotaklogin);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        pindahlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent masuklogin = new Intent(tugas2register.this, tugas3loginactivity.class);
+                startActivity(masuklogin);
+            }
+        });
+
+        pindahregis = findViewById(R.id.register);
+
+        pindahregis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent masukreg = new Intent(tugas2register.this, tugas3register.class);
+                startActivity(masukreg);
+            }
         });
     }
 }
--- app/src/main/java/com/example/tugas2login/tugas3loginactivity.java
+++ app/src/main/java/com/example/tugas2login/tugas3loginactivity.java
@@ -1,9 +1,6 @@
 package com.example.tugas2login;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button pindahactivity;
+public class tugas3loginactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        pindahactivity = findViewById(R.id.btnnext);
-
+        setContentView(R.layout.activity_tugas3login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        pindahactivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent pindahkelogin = new Intent(MainActivity.this, tugas2register.class);
-                startActivity(pindahkelogin);
-            }
-   });
-}
+    }
 }
